68c6c1a2eeed720d2806b85368738773b685e897
TheV <djardim322@gmail.com>
2025-02-21 14:05:13 -0300

    feat: implementadas rotas para gerenciamento de entregas

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const dotenv = require('dotenv')
 dotenv.config()
 
+app.use(express.json())
 const porta = process.env.PORTA
 //sets
 app.set('view engine', 'ejs');
@@ -24,7 +25,7 @@ app.get('/', (req, res) => {
 
 const entregas = []
 
-app.get('/fluxoEntregas', async (req, res) => {
+app.get('/entregas', async (req, res) => {
   try {
       if (entregas.length === 0) 
           {
@@ -40,9 +41,30 @@ app.get('/fluxoEntregas', async (req, res) => {
   }
 });
 
+//Metodo GET pelo id:
+app.get('/entregas/:id', (req, res) => {
+  try {
+    const id = req.params.id;
+    const entrega = entregas.find(elemento => elemento.id === id)
+    
+    if(!entrega){
+      return res.status(404).json({msg:"entrega não encontrado!"})
+    }
+    
+  res.status(200).json(entrega);
+  } 
+  catch (error) {
+  res.status(500).json(
+    {
+      msg: "Erro ao buscar entrega!",
+      erro: error.message
+  })
+  }
+
+});
 
 //Metodos POST pra enviar dados
-app.post('/fluxoEntregas', (req, res) => {
+app.post('/entregas', (req, res) => {
   // Extrai os dados do corpo da requisição
   try {
     const { id, remetente, destinatario, enderecoDestino, dataPrevista, status } = req.body;
@@ -65,8 +87,57 @@ app.post('/fluxoEntregas', (req, res) => {
   }
 });
 
+// Rota para editar a entrega
+app.put('/entregas/:id', (req, res) => {
+  try {
+    // Pegando o id da rota
+    const id = req.params.id;
+    const { novoTitulo, novoAutor, novoAnoPublicacao, novoGenero, novaSinopse } = req.body;
+
+    // Busca o entrega com o id fornecido
+    const entrega = entregas.find(elemento => elemento.id === parseInt(id)); // Usando parseInt(id) para garantir que seja tratado como número
+
+    // Se o entrega não for encontrado
+    if (!id){
+      return res.status(404).json({error: "Informe um parâmetro"})
+    }
+    if (!entrega) {
+      return res.status(404).json({ error: "entrega não encontrada!" });
+    }
+    if (entrega) {
+          // Atualiza os dados do entrega
+    entrega.remetente = novoRemetente || entrega.remetente;
+    entrega.destinatario = novoDestinatario || entrega.destinatario;
+    entrega.enderecoDestino = novoEnderecoDestino || entrega.enderecoDestino;
+    entrega.dataPrevista = novaDataPrevista || entrega.dataPrevista;
+    entrega.status = novoStatus || entrega.status;
+    
+      // Retorna o entrega atualizado
+      return res.status(200).json({msg: 'entrega atualizado com sucesso!'});
+      
+    }
+
+  } catch (error) {
+    return res.status(500).json({ error: "Erro ao atualizar entrega!" });
+  }
+});
+
+//Metodo DELETE para deletar entregas
+
+app.delete("/entregas/:id", (req,res) => {
+  try {
+  const id = req.params.id;
+  const entrega = entregas.findIndex(elemento => elemento.id === parseInt(id))
+  if(entrega === -1){
+    entregas.splice(index, 1)
+    return res.status(404).json({msg: "entrega não encontrada!"})
+  }} 
+  catch (error) {
+    res.status(500).json({msg:"Erro ao deletar o parametro do banco de dados!"})
+  }
+})
 
 
 app.listen(porta, () => {
-  console.log(`App de exemplo esta rodando em http://localhost:${porta}`)
+  console.log(`App de exemplo esta rodando em http://localhost:${porta}, acesse: http://localhost:${porta}`)
 })
